aba21d7760dc300372f83e9e51e18f2d79931bf9
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-06-29 16:38:51 -0700

    my first commit

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models #im guessing this is an existing template?
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Post(models.Model):
+
+    #default attributes
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length= 200)
+    created_date = models.DateTimeField(
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
