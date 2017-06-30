f52402864c941dd03a5ab848c4afdcd1549e8722
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-06-30 14:20:07 -0700

    added tempaltes and html

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #pattern match strings ending with '/'
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post #connect Post model to this view code
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
@@ -10,3 +12,8 @@ def post_list(request): #take a request arguement
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts}) #renders the request on the html page
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
