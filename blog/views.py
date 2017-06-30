766ea660d5f2fd92e488c4a1307d15ba15e73028
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-06-29 19:33:39 -0700

    added post templates to blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def post_list(request): #take a request arguement
+    return render(request, 'blog/post_list.html', {}) #call render
